36648b18db60d368d24eab6e4b12fddee0145c7e
chengqingyuan <2033619233@qq.com>
2024-12-15 17:22:19 +0800

    模板基础实现

--- app/src/main/java/com/example/clientapp/feature/home/HomeRoute.kt
+++ app/src/main/java/com/example/clientapp/feature/home/HomeRoute.kt
@@ -2,10 +2,21 @@ package com.example.clientapp.feature.home
 
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.provider.FontsContractCompat.Columns
+import java.lang.reflect.Modifier
 
 @Composable
 fun HomeRoute() {
@@ -13,9 +24,25 @@ fun HomeRoute() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun homeScreen() {
+
+    var state by remember { mutableStateOf(0) }
+    val titles = listOf("Tab 1", "Tab 2", "Tab 3 ")
+
+
     Column {
+
+        TabRow(selectedTabIndex = state) {
+            titles.forEachIndexed { index, title ->
+                Tab(
+                    selected = state == index,
+                    onClick = { state = index },
+                    text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) }
+                )
+            }
+        }
         Text("home")
     }
 }
--- app/src/main/java/com/example/clientapp/feature/main/MainRoute.kt
+++ app/src/main/java/com/example/clientapp/feature/main/MainRoute.kt
@@ -1,21 +1,23 @@
 package com.example.clientapp.feature.main
 
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Button
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.clientapp.components.NavBar
 import com.example.clientapp.feature.home.HomeRoute
 import com.example.clientapp.feature.my.MyRoute
 import com.example.clientapp.feature.test.TestRoute
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainRoute(finishPage: () -> Unit) {
@@ -25,20 +27,33 @@ fun MainRoute(finishPage: () -> Unit) {
 
 @Composable
 fun MainScreen(finishPage: () -> Unit) {
-    val pagerState = rememberPagerState { 3 }
-    HorizontalPager(state = pagerState, modifier = Modifier.fillMaxWidth()) { page ->
-        when (page) {
-            0 -> HomeRoute()
-            1 -> TestRoute()
-//            2 -> MessageRoute()
-            2 -> MyRoute()
-        }
-    }
-    Button(onClick = { finishPage() }) {
-        Text("退出")
-    }
 
-    NavBar()
+    val scope = rememberCoroutineScope()
+    //   选中项
+    var selectedItem by remember { mutableIntStateOf(0) }
+
+    Column {
+        val pagerState = rememberPagerState { 3 }
+        HorizontalPager(state = pagerState, modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)) { page ->
+            when (page) {
+                0 -> HomeRoute()
+                1 -> TestRoute()
+//            2 -> MessageRoute()
+                2 -> MyRoute()
+            }
+        }
+//        Button(onClick = { finishPage() }) {
+//            Text("退出")
+//        }
+        NavBar(selectedItem, pagerState, onNavSelect = { index ->
+            selectedItem = index
+            scope.launch {
+                pagerState.animateScrollToPage(index)
+            }
+        })
+    }
 
 }
 
